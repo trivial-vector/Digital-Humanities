dd98505aa9bd5d22a49eea61a056877ab8b6708a
Karika Shah <karikashah@yahoo.com>
2019-04-07 07:39:09 -0500

    Merged all Gage's, Justin's & Karika's contents on Staging & incorporated Blueprint concept.

--- Staging/libs/data_engineering.py
+++ Staging/libs/data_engineering.py
@@ -1,14 +1,15 @@
+import utils as vizgjk
+import pymongo
+import pandas as pd
 print("In the data engineering")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~")
 # ===============================================================================================================================
 # All the packages used for data engineering & cleaning part
 # ===============================================================================================================================
-import pandas as pd
-import pymongo
 
 # created our own library
-import lib
-from lib import utils as vizgjk
+# from libs import utils as vizgjk
+
 
 # ===============================================================================================================================
 # Step 1: Initialize PyMongo to work with MongoDBs & create our OWN db to store information
@@ -18,8 +19,9 @@ client = pymongo.MongoClient(conn)
 
 # Retrieve OR Create New database and collection
 db_DH = client.digitalHumanity_db
-years = ["1900","1902","1903","1905","1907","1908","1910","1911","1912","1913","1915","1917","1918","1919","1920"]
-census_years = ["1900","1910","1920"]
+years = ["1900", "1902", "1903", "1905", "1907", "1908", "1910",
+         "1911", "1912", "1913", "1915", "1917", "1918", "1919", "1920"]
+census_years = ["1900", "1910", "1920"]
 
 dict_censuses_col = {}
 
@@ -44,17 +46,19 @@ for y in years:
     censusname = "censusname"+y
     censusrace = "censusrace"+y
 
-    iscensus = 0 # variable to determine if its census year
+    iscensus = 0  # variable to determine if its census year
     for cy in census_years:
         # Check if its the census year
         if(cy == y):
             iscensus = 1
-            break # it should exit the for loop of census year (cy) - which will happen for yrs 1900,1910,1920
+            # it should exit the for loop of census year (cy) - which will happen for yrs 1900,1910,1920
+            break
     if(iscensus):
         # Yes it is census year then we have census race & name column to access
-        df = df_basic_data_allyrs.loc[:,["StNumClean",name,race,tenure,censusname,censusrace]]
+        df = df_basic_data_allyrs.loc[:, [
+            "StNumClean", name, race, tenure, censusname, censusrace]]
     else:
-        df = df_basic_data_allyrs.loc[:,["StNumClean",name,race,tenure]]
+        df = df_basic_data_allyrs.loc[:, ["StNumClean", name, race, tenure]]
     # populate dataframe with the basic info
     df = vizgjk.create_basic_df_for_that_year(df, y, iscensus)
     # push that information in mongo db
@@ -73,7 +77,8 @@ for cy in census_years:
     df = pd.read_csv(filename)
     df_new = vizgjk.create_additional_info_for_that_year(df, cy)
     for index, row in df_new.iterrows():
-        dict_censuses_col[cy].update_many({'Year': row[1], 'Street Address': row[-1], 'Census Name': row[0]}, {"$set": row.to_dict()}, upsert=True)
+        dict_censuses_col[cy].update_many(
+            {'Year': row[1], 'Street Address': row[-1], 'Census Name': row[0]}, {"$set": row.to_dict()}, upsert=True)
 
 # ===============================================================================================================================
 # Step 4: Retrieve the Latitude & Longitude info based on Street Address, Year & Full Name/ Census Name
@@ -84,13 +89,15 @@ filename_latlong = "./Resources/LatLong_Info.csv"
 df_latlong = pd.read_csv(filename_latlong)
 
 # handling inconsistency of Street address across multiple csv files.
-df_latlong["Street Address"] = df_latlong["Street Address"].str.replace("Street","St") 
+df_latlong["Street Address"] = df_latlong["Street Address"].str.replace(
+    "Street", "St")
 
 # Step 4b: insert lat long info into MongoDB collections for different years
 for y in years:
     for row in df_latlong.iterrows():
         data = dict(row[1])
-        dict_censuses_col[y].update_many({'Year': y, 'Street Address': row[1]["Street Address"]}, {"$set": data}, upsert=True)
+        dict_censuses_col[y].update_many(
+            {'Year': y, 'Street Address': row[1]["Street Address"]}, {"$set": data}, upsert=True)
 
 
 print("Done with Data engineering. Check your MongoCompass for 'digitalHumanity_db' database")
