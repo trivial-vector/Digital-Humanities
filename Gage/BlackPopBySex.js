42c1af89e4cc38af7c9c367f98b8c0708a0f5eed
ouroboros <justin.parker.12@gmail.com>
2019-04-10 00:45:59 -0500

    final push

--- Gage/BlackPopBySex.js
+++ Gage/BlackPopBySex.js
@@ -1,35 +1,33 @@
 var trace1 = {
-  x: ['1900', '1910', '1920'], 
-  y: [391, 140, 1220], 
-  name: 'Female', 
+  x: ['1900', '1910', '1920'],
+  y: [391, 140, 1220],
+  name: 'Female',
   type: 'bar'
 };
 
 var trace2 = {
   x: ['1900', '1910', '1920'],
-  y: [338, 98, 1318], 
-  name: 'Male', 
+  y: [338, 98, 1318],
+  name: 'Male',
   type: 'bar'
 };
 
-
 var data = [trace1, trace2];
-var layout = {barmode: 'group'};
+var layout = { barmode: 'group' };
 
 var layout = {
-  title: "Black Population by Gender for Years 1910-1920",
+  title: 'Population Growth by Gender',
 
   xaxis: {
     title: {
-      text: 'Year',
-    },
+      text: 'Year'
+    }
   },
   yaxis: {
     title: {
-      text: 'Count',
+      text: 'Count'
     }
   }
-
 };
 
-Plotly.newPlot('myDiv', data, layout, {}, {showSendToCloud:true});
+Plotly.newPlot('myDiv', data, layout, {}, { showSendToCloud: true });
